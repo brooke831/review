5c982893c00cc0468d2bb5826e4e7ad014286a26
brooke <keaton0831@gmail.com>
2019-06-23 20:35:31 +0800

    toUTF8

---
+++ reviewsv2.py
@@ -0,0 +1,16 @@
+product =[]
+while True:
+    name = input("請輸入商品名稱:")
+    if name =='q':
+        break
+    price = input("請輸入商品價格:")
+    product.append([name,price])
+print(product)
+
+for p in product:
+    print (p[0],"的價格是",p[1])
+    
+with open('product.txt','w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in product :
+        f.write(p[0] +','+p[1]+'\n')
